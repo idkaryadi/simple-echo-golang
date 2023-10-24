4b6572a156b485f582406e4d37b9cafa12873adc
idkaryadi <karyadi@alterra.id>
2023-10-24 10:40:47 +0700

    1 remove .air.toml and update main.go

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func main() {
 	e := echo.New()
 	e.GET("/v1/products", GetProduct)
 	e.GET("/v1/products/:id", GetProductById)
+	e.POST("/v1/products", CreateProduct)
+	e.PUT("/v1/products/:id", UpdateProduct)
+	e.DELETE("/v1/products/:id", DeleteProduct)
 
 	e.Logger.Fatal(e.Start(":4000"))
 }
@@ -21,4 +24,18 @@ func GetProduct(c echo.Context) error {
 func GetProductById(c echo.Context) error {
 	id := c.Param("id")
 	return c.JSON(http.StatusOK, echo.Map{"status": "success", "data": echo.Map{"id": id}})
+}
+
+func CreateProduct(c echo.Context) error {
+	return c.JSON(http.StatusCreated, echo.Map{"status": "success"})
+}
+
+func UpdateProduct(c echo.Context) error {
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, echo.Map{"status": "success", "updateId": id})
+}
+
+func DeleteProduct(c echo.Context) error {
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, echo.Map{"status": "success", "deletedId": id})
 }
