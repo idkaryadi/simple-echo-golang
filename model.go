c1fe4056dbba28da641af2c86c397346c7bfd6e9
idkaryadi <karyadi@alterra.id>
2023-10-24 13:53:38 +0700

    3 create connection to mysql

---
+++ model.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Products struct {
+	ID          int `gorm:"primaryKey,autoIncrement"`
+	Name        string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	DeletedAt   time.Time
+}
+
+var (
+	DB *gorm.DB
+)
+
+func connectDB() {
+	dsn := "root:simple-password@tcp(mysqldb:3306)/simple-project?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Err::", err)
+	}
+
+	// TODO: auto migrate
+	DB.AutoMigrate(&Products{})
+}
