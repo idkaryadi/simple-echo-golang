b4cd03a19c5d1046e013332cb0a0f1d2fe5650a5
idkaryadi <karyadi@alterra.id>
2023-10-25 15:57:49 +0700

    3 connect to backend

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -19,24 +22,93 @@ func main() {
 }
 
 func GetProduct(c echo.Context) error {
-	return c.JSON(http.StatusOK, echo.Map{"status": "success"})
+	var products []Products
+	result := DB.Find(&products)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+	}
+	fmt.Println("products", products, result.Error)
+	return c.JSON(http.StatusOK, echo.Map{"status": "success", "payload": products})
 }
 
 func GetProductById(c echo.Context) error {
-	id := c.Param("id")
-	return c.JSON(http.StatusOK, echo.Map{"status": "success", "data": echo.Map{"id": id}})
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": "failed", "message": "Invalid Id"})
+	}
+	product := Products{ID: id}
+	result := DB.Find(&product)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+	}
+	if result.RowsAffected < 1 {
+		return c.JSON(http.StatusNotFound, echo.Map{"status": "not found", "message": "data not found"})
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{"status": "success", "data": product})
 }
 
 func CreateProduct(c echo.Context) error {
+	req := new(ProductRequest)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": err.Error()})
+	}
+	newProduct := Products{
+		Name:        req.Name,
+		Description: req.Description,
+	}
+	result := DB.Create(&newProduct)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+	}
 	return c.JSON(http.StatusCreated, echo.Map{"status": "success"})
 }
 
 func UpdateProduct(c echo.Context) error {
-	id := c.Param("id")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": "failed", "message": err.Error()})
+	}
+	req := new(ProductRequest)
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": err.Error()})
+	}
+	// get product
+	product := Products{ID: id}
+	resProduct := DB.Find(&product)
+	if resProduct.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": err.Error()})
+	}
+	if resProduct.RowsAffected < 1 {
+		return c.JSON(http.StatusNotFound, echo.Map{"status": "not found", "message": "data not found"})
+	}
+	updateProduct := Products{
+		ID:          id,
+		Name:        req.Name,
+		Description: req.Description,
+		CreatedAt:   product.CreatedAt,
+		UpdatedAt:   time.Now(),
+		// DeletedAt:   product.DeletedAt,
+	}
+	result := DB.Save(&updateProduct)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "error", "message": result.Error.Error()})
+	}
 	return c.JSON(http.StatusOK, echo.Map{"status": "success", "updateId": id})
 }
 
 func DeleteProduct(c echo.Context) error {
-	id := c.Param("id")
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": "invalid id type"})
+	}
+	// Note: masih hard delete
+	result := DB.Delete(&Products{}, id)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+	}
+	if result.RowsAffected < 1 {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": "data not found"})
+	}
 	return c.JSON(http.StatusOK, echo.Map{"status": "success", "deletedId": id})
 }
--- model.go
+++ model.go
@@ -9,21 +9,22 @@ import (
 )
 
 type Products struct {
-	ID          int `gorm:"primaryKey,autoIncrement"`
+	ID          int `gorm:"primaryKey;autoIncrement"`
 	Name        string
 	Description string
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
-	DeletedAt   time.Time
+	// DeletedAt   *time.Time `gorm:"default:NULL"`
 }
 
 var (
-	DB *gorm.DB
+	DB  *gorm.DB
+	err error
 )
 
 func connectDB() {
 	dsn := "root:simple-password@tcp(mysqldb:3306)/simple-project?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Err::", err)
 	}
