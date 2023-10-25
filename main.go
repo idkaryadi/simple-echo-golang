e2fbfa42b421bef26844f04194c8b4c6bbbcf2ba
idkaryadi <karyadi@alterra.id>
2023-10-25 16:24:33 +0700

    3 update product response

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -25,33 +24,32 @@ func GetProduct(c echo.Context) error {
 	var products []Products
 	result := DB.Find(&products)
 	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", result.Error.Error()))
 	}
-	fmt.Println("products", products, result.Error)
-	return c.JSON(http.StatusOK, echo.Map{"status": "success", "payload": products})
+	return c.JSON(http.StatusOK, NewGetProductListResponse(products))
 }
 
 func GetProductById(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"status": "failed", "message": "Invalid Id"})
+		return c.JSON(http.StatusBadRequest, NewErrorResponse("error", "Invalid Id"))
 	}
 	product := Products{ID: id}
 	result := DB.Find(&product)
 	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", result.Error.Error()))
 	}
 	if result.RowsAffected < 1 {
-		return c.JSON(http.StatusNotFound, echo.Map{"status": "not found", "message": "data not found"})
+		return c.JSON(http.StatusNotFound, NewErrorResponse("error", "data not found"))
 	}
 
-	return c.JSON(http.StatusOK, echo.Map{"status": "success", "data": product})
+	return c.JSON(http.StatusOK, NewGetProductResponse(product))
 }
 
 func CreateProduct(c echo.Context) error {
 	req := new(ProductRequest)
 	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": err.Error()})
+		return c.JSON(http.StatusBadRequest, NewErrorResponse("error", err.Error()))
 	}
 	newProduct := Products{
 		Name:        req.Name,
@@ -59,28 +57,28 @@ func CreateProduct(c echo.Context) error {
 	}
 	result := DB.Create(&newProduct)
 	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", result.Error.Error()))
 	}
-	return c.JSON(http.StatusCreated, echo.Map{"status": "success"})
+	return c.JSON(http.StatusCreated, NewSuccessPayload())
 }
 
 func UpdateProduct(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"status": "failed", "message": err.Error()})
+		return c.JSON(http.StatusBadRequest, NewErrorResponse("error", err.Error()))
 	}
 	req := new(ProductRequest)
 	if err := c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": err.Error()})
+		return c.JSON(http.StatusBadRequest, NewErrorResponse("error", err.Error()))
 	}
 	// get product
 	product := Products{ID: id}
 	resProduct := DB.Find(&product)
 	if resProduct.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": err.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", err.Error()))
 	}
 	if resProduct.RowsAffected < 1 {
-		return c.JSON(http.StatusNotFound, echo.Map{"status": "not found", "message": "data not found"})
+		return c.JSON(http.StatusNotFound, NewErrorResponse("error", "data not found"))
 	}
 	updateProduct := Products{
 		ID:          id,
@@ -92,23 +90,23 @@ func UpdateProduct(c echo.Context) error {
 	}
 	result := DB.Save(&updateProduct)
 	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "error", "message": result.Error.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", result.Error.Error()))
 	}
-	return c.JSON(http.StatusOK, echo.Map{"status": "success", "updateId": id})
+	return c.JSON(http.StatusOK, NewSuccessPayload())
 }
 
 func DeleteProduct(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, echo.Map{"status": "Error", "message": "invalid id type"})
+		return c.JSON(http.StatusBadRequest, NewErrorResponse("error", err.Error()))
 	}
 	// Note: masih hard delete
 	result := DB.Delete(&Products{}, id)
 	if result.Error != nil {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": result.Error.Error()})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", result.Error.Error()))
 	}
 	if result.RowsAffected < 1 {
-		return c.JSON(http.StatusInternalServerError, echo.Map{"status": "Error", "message": "data not found"})
+		return c.JSON(http.StatusInternalServerError, NewErrorResponse("error", "data not found"))
 	}
-	return c.JSON(http.StatusOK, echo.Map{"status": "success", "deletedId": id})
+	return c.JSON(http.StatusOK, NewSuccessPayload())
 }
