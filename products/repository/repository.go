b227a94bc368f9bb5ad4224bd6ce3bd706de25cc
idkaryadi <karyadi@alterra.id>
2023-10-26 13:27:28 +0700

    8 implement struct

--- products/repository/repository.go
+++ products/repository/repository.go
@@ -2,38 +2,57 @@ package repository
 
 import (
 	"errors"
-	"simple-echo-backend/config"
+	"time"
+
+	"gorm.io/gorm"
 )
 
-func GetProducts() ([]Products, error) {
+type Repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return Repository{db: db}
+}
+
+type Products struct {
+	ID          int `gorm:"primaryKey;autoIncrement"`
+	Name        string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	// DeletedAt   *time.Time `gorm:"default:NULL"`
+}
+
+func (repo Repository) GetProducts() ([]Products, error) {
 	var products []Products
-	result := config.DB.Find(&products)
+	result := repo.db.Find(&products)
 
 	return products, result.Error
 }
 
-func GetProductById(id int) (Products, error) {
+func (repo Repository) GetProductById(id int) (Products, error) {
 	product := Products{ID: id}
-	result := config.DB.Find(&product)
+	result := repo.db.Find(&product)
 	if result.RowsAffected < 1 {
 		return product, errors.New("data not found")
 	}
 	return product, result.Error
 }
 
-func CreateProduct(newProduct Products) error {
-	result := config.DB.Create(&newProduct)
+func (repo Repository) CreateProduct(newProduct Products) error {
+	result := repo.db.Create(&newProduct)
 	return result.Error
 }
 
-func UpdateProduct(updateProduct Products) error {
-	result := config.DB.Save(&updateProduct)
+func (repo Repository) UpdateProduct(updateProduct Products) error {
+	result := repo.db.Save(&updateProduct)
 	return result.Error
 }
 
-func DeleteProduct(id int) error {
+func (repo Repository) DeleteProduct(id int) error {
 	// Note: masih hard delete
-	result := config.DB.Delete(&Products{}, id)
+	result := repo.db.Delete(&Products{}, id)
 	if result.RowsAffected < 1 {
 		return errors.New("data not found")
 	}
